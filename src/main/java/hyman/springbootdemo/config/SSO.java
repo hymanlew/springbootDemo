5b49090861029f086855bf74d1dd0019aebf6f16
hyman <hyman@163.com>
2019-07-21 22:52:48 +0800

    update sso

---
+++ src/main/java/hyman/springbootdemo/config/SSO.java
@@ -0,0 +1,14 @@
+package hyman.springbootdemo.config;
+
+public class SSO {
+
+    /**
+     *
+     security + JWT + Oauth2：
+     附上源码：  https://github.com/chengjiansheng/cjs-oauth2-sso-demo.git
+
+     security + redis + Oauth2：
+     https://blog.csdn.net/leipeng321123/article/details/88951755
+
+     */
+}
