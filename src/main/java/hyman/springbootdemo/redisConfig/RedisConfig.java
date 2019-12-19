27008583e23d7f5326219827d10424b866e49c6f
hymanlew <wohena3689>
2019-12-20 00:21:56 +0800

    update redis

--- src/main/java/hyman/springbootdemo/redisConfig/RedisConfig.java
+++ src/main/java/hyman/springbootdemo/redisConfig/RedisConfig.java
@@ -14,12 +14,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -104,11 +102,6 @@ public class RedisConfig {
         Jackson2JsonRedisSerializer<User> serializer = new Jackson2JsonRedisSerializer(User.class);
         template.setDefaultSerializer(serializer);
         return template;
-    }
-
-    @Bean
-    public ReactiveStringRedisTemplate reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
-        return new ReactiveStringRedisTemplate(factory);
     }
 
     /**
